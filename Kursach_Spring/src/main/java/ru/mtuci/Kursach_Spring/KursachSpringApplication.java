bc2e9fa41491232c72f66f8817c18436dcb199db
serdyukdan <serdyukdan@gmail.com>
2023-09-18 17:55:26 +0300

    Added Spring base

---
+++ Kursach_Spring/src/main/java/ru/mtuci/Kursach_Spring/KursachSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.mtuci.Kursach_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursachSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursachSpringApplication.class, args);
+	}
+
+}
---
+++ Kursach_Spring/src/test/java/ru/mtuci/Kursach_Spring/KursachSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtuci.Kursach_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursachSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
